3beb85e3bf6c6f200693e1d2701c20354caeadd8
Cyril HERON <cheron@e1r5p14.42.fr>
2015-01-11 19:40:23 +0100

    d06 commit before night

--- d06/ex05/Render.class.php
+++ d06/ex05/Render.class.php
@@ -23,12 +23,42 @@ Class Render {
 		return;
 	}
 
+	public function renderSegment( Vertex $start, Vertex $end ) {
+		$destx = ( ( $start->getX() - $end->getX() >= 0 ) ? ( $start->getX() - $end->getX() ) : ( $end->getX() - $start->getX() ) );
+		$desty = ( $start->getY() - $end->getY() >= 0 ? $start->getY() - $end->getY() : $end->getY() - $start->getY() );
+		$sourcex = ( $start->getX() < $end->getX() ? 1 : -1);
+		$sourcey = ( $start->getY() < $end->getY() ? 1 : -1);
+		$errx = ( $destx > $destdy ? $destx : -$desty ) / 2;
+		while ( $start->getX() != $end->getY() || $start->getY() != $end->getY() )
+		{
+			$this->renderVertex( $start );
+			$erry = $errx;
+			if ( $erry > -$destx )
+			{
+				echo ("lol");
+				$errx = $errx - $desty;
+				$start->setX( $start->getX() + $sourcex );
+			}
+			if ($erry < $desty)
+			{
+				echo("lol");
+				$errx = $errx + $destx;
+				$start->setY( $start->getY() + $sourcey );
+			}
+		}
+		return;
+	}
 	public function renderTriangle( Triangle $triangle, $mode ) {
 		if ( $triangle->getVisibility() === false )
 			return;
-		$this->renderVertex( $triangle->getA() );	
+		$this->renderVertex( $triangle->getA() );
 		$this->renderVertex( $triangle->getB() );
 		$this->renderVertex( $triangle->getC() );
+		if ( $mode > 0 ) {
+		$this->renderSegment( $triangle->getB(), $triangle->getA() );
+		$this->renderSegment( $triangle->getB(), $triangle->getC() );
+		$this->renderSegment( $triangle->getA(), $triangle->getC() );
+		}	
 		return;
 	}
 
